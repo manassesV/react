0b232e95b4c71f9002e33b1ec08faefed5800970
Manassés <manavitorino@gmail.com>
2019-05-20 22:38:41 -0300

    Add files via upload

---
+++ precache-manifest.d7b262837ea592dc243dbcc211e78570.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816f8796a3f28191a20a037607e59690",
+    "url": "/react/fiap-react-17mob/index.html"
+  },
+  {
+    "revision": "e934c4de3afcfc022f64",
+    "url": "/react/fiap-react-17mob/static/css/main.614bef7f.chunk.css"
+  },
+  {
+    "revision": "2c0a5c5086389b9c36b4",
+    "url": "/react/fiap-react-17mob/static/js/2.aa6e1a06.chunk.js"
+  },
+  {
+    "revision": "e934c4de3afcfc022f64",
+    "url": "/react/fiap-react-17mob/static/js/main.9bfcd2b1.chunk.js"
+  },
+  {
+    "revision": "cda47474200bd77ba8ce",
+    "url": "/react/fiap-react-17mob/static/js/runtime~main.e755f8cd.js"
+  }
+]);
